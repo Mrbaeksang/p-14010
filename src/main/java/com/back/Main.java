0da27cc0e9c5876c98d984a42f9a5f1197be5ff2
Mrbaeksang <qortkdgus95@gmail.com>
2025-05-22 15:25:16 +0900

    6단계

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -7,41 +7,59 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         List<Quote> quotes = new ArrayList<>();
-
-        int id = 1; // 명언 넘버링
+        int id = 1;
 
         System.out.println("== 명언 앱 ==");
 
         while (true) {
-            System.out.println("명령)");
-
+            System.out.print("명령) ");
             String command = sc.nextLine();
 
             switch (command) {
                 case "등록":
-                    System.out.println("명언을 입력하세요: ");
+                    System.out.print("명언을 입력하세요: ");
                     String content = sc.nextLine();
-                    System.out.println("작가를 입력하세요: ");
+
+                    System.out.print("작가를 입력하세요: ");
                     String author = sc.nextLine();
 
-                    Quote q = new Quote(id, content, author);
-                    quotes.add(q);
+                    quotes.add(new Quote(id, content, author));
 
                     System.out.println("명언 : " + content);
                     System.out.println("작가 : " + author);
-                    System.out.println(id+"번 명언이 등록되었습니다.");
+                    System.out.println(id + "번 명언이 등록되었습니다.");
                     id++;
                     break;
 
                 case "목록":
-                    System.out.println("번호 / 작가 / 명언 ");
+                    System.out.println("번호 / 작가 / 명언");
                     System.out.println("--------------------");
-                    for(Quote quote : quotes) {
+                    for (Quote quote : quotes) {
                         System.out.println(quote.id + " / " + quote.author + " / " + quote.content);
                     }
                     break;
+
+                case "삭제":
+                    System.out.print("삭제할 번호를 입력하세요: ");
+                    int num = sc.nextInt();
+                    sc.nextLine();
+
+                    boolean found = false;
+                    for (int i = 0; i < quotes.size(); i++) {
+                        if (quotes.get(i).id == num) {
+                            quotes.remove(i);
+                            System.out.println(num + "번 명언이 삭제되었습니다.");
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println(num + "번 명언은 존재하지 않습니다.");
+                    }
+                    break;
+
                 case "종료":
                     System.out.println("프로그램을 종료합니다.");
                     return;
@@ -49,11 +67,10 @@ public class Main {
                 default:
                     System.out.println("올바른 명령어가 아닙니다.");
             }
-
-
         }
     }
 }
+
 class Quote {
     int id;
     String content;
