c587cfa689b273788f50b8a95a8777c282517c0b
Mrbaeksang <qortkdgus95@gmail.com>
2025-05-22 13:24:37 +0900

    5단계

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -1,11 +1,16 @@
 package com.back;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int i = 1; // 명언 넘버링
+
+        List<Quote> quotes = new ArrayList<>();
+
+        int id = 1; // 명언 넘버링
 
         System.out.println("== 명언 앱 ==");
 
@@ -17,14 +22,25 @@ public class Main {
             switch (command) {
                 case "등록":
                     System.out.println("명언을 입력하세요: ");
-                    String speak = sc.nextLine();
+                    String content = sc.nextLine();
                     System.out.println("작가를 입력하세요: ");
                     String author = sc.nextLine();
-                    System.out.println("등록되었습니다 !");
-                    System.out.println("명언 : " + speak);
+
+                    Quote q = new Quote(id, content, author);
+                    quotes.add(q);
+
+                    System.out.println("명언 : " + content);
                     System.out.println("작가 : " + author);
-                    System.out.println(i+"번 명언이 등록되었습니다.");
-                    i++;
+                    System.out.println(id+"번 명언이 등록되었습니다.");
+                    id++;
+                    break;
+
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언 ");
+                    System.out.println("--------------------");
+                    for(Quote quote : quotes) {
+                        System.out.println(quote.id + " / " + quote.author + " / " + quote.content);
+                    }
                     break;
                 case "종료":
                     System.out.println("프로그램을 종료합니다.");
@@ -36,5 +52,16 @@ public class Main {
 
 
         }
+    }
+}
+class Quote {
+    int id;
+    String content;
+    String author;
+
+    public Quote(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
     }
 }
