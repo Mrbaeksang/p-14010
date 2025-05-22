242dfb50b173cdb9444ec25267afda7687636816
Mrbaeksang <qortkdgus95@gmail.com>
2025-05-22 12:25:38 +0900

    2단계

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -7,10 +7,31 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
-        System.out.println("명령)");
-        int num = 2;
+
+        while (true) {
+            System.out.println("명령)");
+
+            String command = sc.nextLine();
+
+            switch (command) {
+                case "등록":
+                    System.out.println("명언을 입력하세요: ");
+                    String speak = sc.nextLine();
+                    System.out.println("작가를 입력하세요: ");
+                    String author = sc.nextLine();
+                    System.out.println("등록되었습니다 !");
+                    System.out.println("명언 : " + speak);
+                    System.out.println("작가 : " + author);
+                    break;
+                case "종료":
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
+
+                default:
+                    System.out.println("올바른 명령어가 아닙니다.");
+            }
 
 
-
+        }
     }
 }
